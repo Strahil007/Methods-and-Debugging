01ed0ba67aa47479e9c59842ce24d264ef0d7b7b
Strahil007 <strahil007@abv.bg>
2016-10-01 18:08:48 +0300

    lab

---
+++ Lab/Greater of Two Values.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greater_of_Two_Values
+{
+    class GreaterofTwoValues
+    {
+        static int GetMax(int first, int second)
+        {
+            if (first >= second)
+            {
+                return GetMax;
+            }
+        }
+        static char GetMax(char first, char second)
+        {
+            if ()
+            {
+
+            }
+        }
+        static string GetMax(string first, string second)
+        {
+            if (first.CompareTo(second) >= 0)
+            {
+                return GetMax;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var type = Console.ReadLine();
+            if (type == "int")
+            {
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+                int max = GetMax(first, second);
+                Console.WriteLine(max);
+            }
+            else if (type == "char")
+            {
+            }
+            else if (type == "string")
+            {
+            }
+        }
+    }
+}
---
+++ Lab/Multiply Evens by Odds.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiply_Evens_by_Odds
+{
+    class MultiplyEvensbyOdds
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+        }
+        private static int GetMultipleOfEvensAndOdds(int n)
+        {
+            int sumEvens = GetSumOfEvenDigits(n);
+            int sumOdds = GetSumOfOddDigits(n);
+            return sumEvens * sumOdds;
+        }
+        private static int GetSumOfOddDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+        private static int GetSumOfEvenDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 1)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
